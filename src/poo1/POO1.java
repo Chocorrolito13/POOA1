5780e8c38119908708d08ec89cf97e91a035be48
jofrd <jofrd@DESKTOP-JQ1GQT6>
2022-11-16 00:42:39 -0700

    Prueba numero 1

--- src/poo1/POO1.java
+++ src/poo1/POO1.java
@@ -15,7 +15,8 @@ public class POO1 {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+   System.out.println("hola mudito");
+   System.out.println("hola prieto");
   }
   
 }
